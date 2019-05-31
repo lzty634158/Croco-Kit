5e0f2bf439501019fcc6c6ecffcb16f9ec41b554
lzty634158 <lzty634158@github.com>
2019-05-31 15:20:17 +0800

    fix sensor

--- main.ts
+++ main.ts
@@ -69,19 +69,28 @@ namespace CrocoKit_Sensor {
         let buff_R = pins.createBuffer(2);
         let buff_G = pins.createBuffer(2);
         let buff_B = pins.createBuffer(2);
+        
+        buff_R[0] = i2cread(COLOR_ADD, COLOR_R);
+        buff_R[1] = i2cread(COLOR_ADD, 0x11);
+        
+        buff_G[0] = i2cread(COLOR_ADD, COLOR_G);
+        buff_G[1] = i2cread(COLOR_ADD, 0x0e);
+        
+        buff_B[0] = i2cread(COLOR_ADD, COLOR_B);
+        buff_B[1] = i2cread(COLOR_ADD, 0x14);
 
-        pins.i2cWriteNumber(COLOR_ADD, COLOR_R, NumberFormat.UInt8BE);
-        buff_R = pins.i2cReadBuffer(COLOR_ADD, 2);
+        // pins.i2cWriteNumber(COLOR_ADD, COLOR_R, NumberFormat.UInt8BE);
+        // buff_R = pins.i2cReadBuffer(COLOR_ADD, 2);
 
-        pins.i2cWriteNumber(COLOR_ADD, COLOR_G, NumberFormat.UInt8BE);
-        buff_G = pins.i2cReadBuffer(COLOR_ADD, 2);
+        // pins.i2cWriteNumber(COLOR_ADD, COLOR_G, NumberFormat.UInt8BE);
+        // buff_G = pins.i2cReadBuffer(COLOR_ADD, 2);
 
-        pins.i2cWriteNumber(COLOR_ADD, COLOR_B, NumberFormat.UInt8BE);
-        buff_B = pins.i2cReadBuffer(COLOR_ADD, 2);
+        // pins.i2cWriteNumber(COLOR_ADD, COLOR_B, NumberFormat.UInt8BE);
+        // buff_B = pins.i2cReadBuffer(COLOR_ADD, 2);
 
-        let Red = 255; //(buff_R[1] & 0xff) << 8 | (buff_R[0] & 0xff);
-        let Green = 0; //(buff_G[1] & 0xff) << 8 | (buff_G[0] & 0xff);
-        let Blue = 0;  //(buff_B[1] & 0xff) << 8 | (buff_B[0] & 0xff);
+        let Red = (buff_R[1] & 0xff) << 8 | (buff_R[0] & 0xff);
+        let Green = (buff_G[1] & 0xff) << 8 | (buff_G[0] & 0xff);
+        let Blue = (buff_B[1] & 0xff) << 8 | (buff_B[0] & 0xff);
 
         if (Red > 4500) Red = 2300;
         if (Green > 7600) Green = 4600;
