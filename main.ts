e62a632605fe4f3ca36a3acaa7c3625729cb38a8
lzty634158 <lzty634158@github.com>
2019-05-31 14:33:39 +0800

    fix sensor

--- main.ts
+++ main.ts
@@ -35,7 +35,25 @@ namespace CrocoKit_Sensor {
         //% blockId="GetValueB" block="GetValueB"
         GetValueB = 2
     }
-    
+    function i2cwrite(addr: number, reg: number, value: number) {
+        let buf = pins.createBuffer(2)
+        buf[0] = reg
+        buf[1] = value
+        pins.i2cWriteBuffer(addr, buf)
+    }
+
+    function i2ccmd(addr: number, value: number) {
+        let buf = pins.createBuffer(1)
+        buf[0] = value
+        pins.i2cWriteBuffer(addr, buf)
+    }
+
+    function i2cread(addr: number, reg: number) {
+        pins.i2cWriteNumber(addr, reg, NumberFormat.UInt8BE);
+        let val = pins.i2cReadNumber(addr, NumberFormat.UInt8BE);
+        return val;
+    }
+
     //% blockId=CrocoKit_Sensor_Light block="Light|pin %pin"
     //% weight=100
     //% blockGap=20
