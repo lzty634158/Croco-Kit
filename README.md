2a181955412e937a647f0f121c4744bbb7fc23b0
lzty634158 <lzty634158@github.com>
2019-07-09 17:22:51 +0800

    fix_csb

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # YB_Croco:Kit
 
-Extension for Yahboom YB_Croco:Kit V1.0
+Extension for Yahboom YB_Croco:Kit V1.1
 
 ## License
 
--- main.ts
+++ main.ts
@@ -78,6 +78,10 @@ namespace CrocoKit_Sensor {
         val_green = Math.map(Green, 0, 4600, 0, 255);
         val_blue = Math.map(Blue, 0, 2700, 0, 255);
 
+        if (val_red > 255) val_red = 255;
+        if (val_green > 255) val_green = 255;
+        if (val_blue > 255) val_blue = 255;
+
         if (val_red == val_green && val_red == val_blue) {
             val_red = 255;
             val_green = 255;
@@ -152,21 +156,18 @@ namespace CrocoKit_Sensor {
     //% blockGap=20
     //% name.fieldEditor="gridpicker" name.fieldOptions.columns=5
     export function Ultrasonic(Trig: DigitalPin, Echo: DigitalPin): number {
-        let list: Array<number> = [0, 0, 0, 0, 0];
-        for (let i = 0; i < 5; i++) {
-            pins.setPull(Trig, PinPullMode.PullNone);
-            pins.digitalWritePin(Trig, 0);
-            control.waitMicros(2);
-            pins.digitalWritePin(Trig, 1);
-            control.waitMicros(15);
-            pins.digitalWritePin(Trig, 0);
+        //send pulse
+        pins.setPull(Trig, PinPullMode.PullNone);
+        pins.digitalWritePin(Trig, 0);
+        control.waitMicros(2);
+        pins.digitalWritePin(Trig, 1);
+        control.waitMicros(10);
+        pins.digitalWritePin(Trig, 0);
 
-            let d = pins.pulseIn(Echo, PulseValue.High, 43200);
-            list[i] = Math.floor(d / 40);
-        }
-        list.sort();
-        let length = (list[1] + list[2] + list[3]) / 3;
-        return Math.floor(length);
+        //read pulse, maximum distance=500cm
+        const d = pins.pulseIn(Echo, PulseValue.High, 500 * 58);   
+
+        return Math.idiv(d, 58);
     }
 
 
